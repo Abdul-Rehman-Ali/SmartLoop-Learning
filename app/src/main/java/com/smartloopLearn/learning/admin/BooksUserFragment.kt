776c66ce175ae8ffb1a3bdb73e678086fe246bab
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2025-04-26 22:54:38 +0500

    c

--- app/src/main/java/com/smartloopLearn/learning/admin/BooksUserFragment.kt
+++ app/src/main/java/com/smartloopLearn/learning/admin/BooksUserFragment.kt
@@ -1,3 +1,173 @@
+//package com.smartloopLearn.learning.admin
+//
+//import android.os.Bundle
+//import android.util.Log
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.Fragment
+//import com.smartloopLearn.learning.databinding.FragmentBooksUserBinding
+//import com.google.firebase.database.DataSnapshot
+//import com.google.firebase.database.DatabaseError
+//import com.google.firebase.database.FirebaseDatabase
+//import com.google.firebase.database.ValueEventListener
+//
+//class BooksUserFragment : Fragment {
+//
+//    private var _binding: FragmentBooksUserBinding? = null
+//    private val binding get() = _binding!!
+//
+//    companion object {
+//        private const val TAG = "Book_USER_TAG"
+//
+//        public fun newInstance(categoryId:String, category: String, uid:String ): BooksUserFragment{
+//            val fragment =  BooksUserFragment()
+//            val args = Bundle()
+//            args.putString("categoryId",categoryId)
+//            args.putString("category",category)
+//            args.putString("uid",uid)
+//            fragment.arguments = args
+//            return fragment
+//        }
+//    }
+//
+//    private var categoryId = ""
+//    private var category = ""
+//    private var uid = ""
+//
+//    private lateinit var pdfArrayList: ArrayList<ModelPdf>
+//    private lateinit var adapterPdfUser: AdapterPdfUser
+//
+//    constructor()
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        val args = arguments
+//        if(args != null){
+//            categoryId = args.getString("categoryId")!!
+//            category = args.getString("category")!!
+//            uid = args.getString("uid")!!
+//        }
+//    }
+//
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout using view binding
+//        _binding = FragmentBooksUserBinding.inflate(inflater, container, false)
+//
+//        Log.d(TAG, "onCreateView: $category")
+//        if (category == "All"){
+//            loadsAllBook()
+//        } else if (category == "Most Viewed") {
+//            loadMostViewDownloadBooks("viewCount")
+//
+//        } else if (category == "Most Downloaded"){
+//            loadMostViewDownloadBooks("downloadCount")
+//
+//        } else {
+//            loadCategorizedBooks()
+//        }
+//
+////        binding.etSearch.addTextChangedListener {
+////            object  : TextWatcher {
+////                override fun beforeTextChanged(
+////                    s: CharSequence?, start: Int, count: Int, after: Int
+////                ) {
+////
+////                }
+////                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+////                    try {
+////                        adapterPdfUser.filter.filter(s)
+////                    } catch (e:Exception){
+////                        Log.d(TAG, "onTextChanged: Search Exception ${e.message}")
+////                    }
+////                }
+////
+////                override fun afterTextChanged(s: Editable?) {
+////
+////                }
+////            }
+////        }
+//
+//        return binding.root
+//
+//    }
+//
+//    private fun loadsAllBook() {
+//        pdfArrayList = ArrayList()
+//        val ref = FirebaseDatabase.getInstance().getReference("Book")
+//        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                pdfArrayList.clear()
+//                for(ds in snapshot.children){
+//                    val model = ds.getValue(ModelPdf::class.java)
+//                    pdfArrayList.add(model!!)
+//                }
+//                adapterPdfUser = AdapterPdfUser(context!!, pdfArrayList)
+//                binding.rvBooks.adapter = adapterPdfUser
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//
+//            }
+//        })
+//    }
+//
+//    private fun loadMostViewDownloadBooks(orderBy: String) {
+//        pdfArrayList = ArrayList()
+//        val ref = FirebaseDatabase.getInstance().getReference("Book")
+//        ref.orderByChild(orderBy).limitToLast(10)
+//            .addListenerForSingleValueEvent(object : ValueEventListener{
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                pdfArrayList.clear()
+//                for(ds in snapshot.children){
+//                    val model = ds.getValue(ModelPdf::class.java)
+//                    pdfArrayList.add(model!!)
+//                }
+//                adapterPdfUser = AdapterPdfUser(context!!, pdfArrayList)
+//                binding.rvBooks.adapter = adapterPdfUser
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//
+//            }
+//        })
+//    }
+//
+//
+//
+//    private fun loadCategorizedBooks() {
+//        pdfArrayList = ArrayList()
+//        val ref = FirebaseDatabase.getInstance().getReference("Book")
+//        ref.orderByChild("Category").equalTo(categoryId)
+//            .addListenerForSingleValueEvent(object : ValueEventListener{
+//                override fun onDataChange(snapshot: DataSnapshot) {
+//                    pdfArrayList.clear()
+//                    for(ds in snapshot.children){
+//                        val model = ds.getValue(ModelPdf::class.java)
+//                        pdfArrayList.add(model!!)
+//                    }
+//                    adapterPdfUser = AdapterPdfUser(context!!, pdfArrayList)
+//                    binding.rvBooks.adapter = adapterPdfUser
+//                }
+//
+//                override fun onCancelled(error: DatabaseError) {
+//
+//                }
+//            })
+//    }
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+//}
+
+
 package com.smartloopLearn.learning.admin
 
 import android.os.Bundle
@@ -5,6 +175,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.smartloopLearn.learning.databinding.FragmentBooksUserBinding
 import com.google.firebase.database.DataSnapshot
@@ -12,7 +183,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-class BooksUserFragment : Fragment {
+class BooksUserFragment : Fragment() {
 
     private var _binding: FragmentBooksUserBinding? = null
     private val binding get() = _binding!!
@@ -20,12 +191,12 @@ class BooksUserFragment : Fragment {
     companion object {
         private const val TAG = "Book_USER_TAG"
 
-        public fun newInstance(categoryId:String, category: String, uid:String ): BooksUserFragment{
-            val fragment =  BooksUserFragment()
+        fun newInstance(categoryId: String, category: String, uid: String): BooksUserFragment {
+            val fragment = BooksUserFragment()
             val args = Bundle()
-            args.putString("categoryId",categoryId)
-            args.putString("category",category)
-            args.putString("uid",uid)
+            args.putString("categoryId", categoryId)
+            args.putString("category", category)
+            args.putString("uid", uid)
             fragment.arguments = args
             return fragment
         }
@@ -38,81 +209,61 @@ class BooksUserFragment : Fragment {
     private lateinit var pdfArrayList: ArrayList<ModelPdf>
     private lateinit var adapterPdfUser: AdapterPdfUser
 
-    constructor()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val args = arguments
-        if(args != null){
-            categoryId = args.getString("categoryId")!!
-            category = args.getString("category")!!
-            uid = args.getString("uid")!!
+        if (args != null) {
+            categoryId = args.getString("categoryId") ?: ""
+            category = args.getString("category") ?: ""
+            uid = args.getString("uid") ?: ""
         }
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout using view binding
+    ): View {
         _binding = FragmentBooksUserBinding.inflate(inflater, container, false)
 
-        Log.d(TAG, "onCreateView: $category")
-        if (category == "All"){
-            loadsAllBook()
-        } else if (category == "Most Viewed") {
-            loadMostViewDownloadBooks("viewCount")
+        Log.d(TAG, "onCreateView: category=$category")
 
-        } else if (category == "Most Downloaded"){
-            loadMostViewDownloadBooks("downloadCount")
-
-        } else {
-            loadCategorizedBooks()
+        when (category) {
+            "All" -> loadsAllBook()
+            "Most Viewed" -> loadMostViewDownloadBooks("viewCount")
+            "Most Downloaded" -> loadMostViewDownloadBooks("downloadCount")
+            else -> loadCategorizedBooks()
         }
 
-//        binding.etSearch.addTextChangedListener {
-//            object  : TextWatcher {
-//                override fun beforeTextChanged(
-//                    s: CharSequence?, start: Int, count: Int, after: Int
-//                ) {
-//
-//                }
-//                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                    try {
-//                        adapterPdfUser.filter.filter(s)
-//                    } catch (e:Exception){
-//                        Log.d(TAG, "onTextChanged: Search Exception ${e.message}")
-//                    }
-//                }
-//
-//                override fun afterTextChanged(s: Editable?) {
-//
-//                }
-//            }
-//        }
+        binding.etSearch.addTextChangedListener { text ->
+            try {
+                adapterPdfUser.filter.filter(text)
+            } catch (e: Exception) {
+                Log.e(TAG, "Search Error: ${e.message}")
+            }
+        }
 
         return binding.root
-
     }
 
     private fun loadsAllBook() {
         pdfArrayList = ArrayList()
         val ref = FirebaseDatabase.getInstance().getReference("Book")
-        ref.addListenerForSingleValueEvent(object : ValueEventListener{
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 pdfArrayList.clear()
-                for(ds in snapshot.children){
+                for (ds in snapshot.children) {
                     val model = ds.getValue(ModelPdf::class.java)
-                    pdfArrayList.add(model!!)
+                    if (model != null) {
+                        pdfArrayList.add(model)
+                    }
                 }
-                adapterPdfUser = AdapterPdfUser(context!!, pdfArrayList)
+                adapterPdfUser = AdapterPdfUser(requireContext(), pdfArrayList)
                 binding.rvBooks.adapter = adapterPdfUser
             }
 
             override fun onCancelled(error: DatabaseError) {
-
+                Log.e(TAG, "loadsAllBook Cancelled: ${error.message}")
             }
         })
     }
@@ -121,42 +272,44 @@ class BooksUserFragment : Fragment {
         pdfArrayList = ArrayList()
         val ref = FirebaseDatabase.getInstance().getReference("Book")
         ref.orderByChild(orderBy).limitToLast(10)
-            .addListenerForSingleValueEvent(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                pdfArrayList.clear()
-                for(ds in snapshot.children){
-                    val model = ds.getValue(ModelPdf::class.java)
-                    pdfArrayList.add(model!!)
-                }
-                adapterPdfUser = AdapterPdfUser(context!!, pdfArrayList)
-                binding.rvBooks.adapter = adapterPdfUser
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-        })
-    }
-
-
-
-    private fun loadCategorizedBooks() {
-        pdfArrayList = ArrayList()
-        val ref = FirebaseDatabase.getInstance().getReference("Book")
-        ref.orderByChild("Category").equalTo(categoryId)
-            .addListenerForSingleValueEvent(object : ValueEventListener{
+            .addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
                     pdfArrayList.clear()
-                    for(ds in snapshot.children){
+                    for (ds in snapshot.children) {
                         val model = ds.getValue(ModelPdf::class.java)
-                        pdfArrayList.add(model!!)
+                        if (model != null) {
+                            pdfArrayList.add(model)
+                        }
                     }
-                    adapterPdfUser = AdapterPdfUser(context!!, pdfArrayList)
+                    adapterPdfUser = AdapterPdfUser(requireContext(), pdfArrayList)
                     binding.rvBooks.adapter = adapterPdfUser
                 }
 
                 override fun onCancelled(error: DatabaseError) {
+                    Log.e(TAG, "loadMostViewDownloadBooks Cancelled: ${error.message}")
+                }
+            })
+    }
 
+    private fun loadCategorizedBooks() {
+        pdfArrayList = ArrayList()
+        val ref = FirebaseDatabase.getInstance().getReference("Book")
+        ref.orderByChild("categoryId").equalTo(categoryId) // Check if your database uses "categoryId" or "Category"
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    pdfArrayList.clear()
+                    for (ds in snapshot.children) {
+                        val model = ds.getValue(ModelPdf::class.java)
+                        if (model != null) {
+                            pdfArrayList.add(model)
+                        }
+                    }
+                    adapterPdfUser = AdapterPdfUser(requireContext(), pdfArrayList)
+                    binding.rvBooks.adapter = adapterPdfUser
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e(TAG, "loadCategorizedBooks Cancelled: ${error.message}")
                 }
             })
     }
--- app/src/main/java/com/smartloopLearn/learning/admin/DashboardUserActivity.kt
+++ app/src/main/java/com/smartloopLearn/learning/admin/DashboardUserActivity.kt
@@ -1,3 +1,143 @@
+//package com.smartloopLearn.learning.admin
+//
+//import android.content.Context
+//import android.os.Bundle
+//import android.view.WindowManager
+//import androidx.activity.enableEdgeToEdge
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.fragment.app.Fragment
+//import androidx.fragment.app.FragmentManager
+//import androidx.fragment.app.FragmentPagerAdapter
+//import androidx.viewpager.widget.ViewPager
+//import com.google.firebase.auth.FirebaseAuth
+//import com.smartloopLearn.learning.databinding.ActivityDashboardUserBinding
+//import com.google.firebase.database.DataSnapshot
+//import com.google.firebase.database.DatabaseError
+//import com.google.firebase.database.FirebaseDatabase
+//import com.google.firebase.database.ValueEventListener
+//
+//class DashboardUserActivity : AppCompatActivity() {
+//
+//    private lateinit var binding: ActivityDashboardUserBinding
+//    private lateinit var categoryArrayList: ArrayList<ModelCategorey>
+//    private lateinit var viewPagerAdapter: ViewPagerAdapter
+//    private lateinit var  firebaseAuth : FirebaseAuth
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        // Prevent screenshots and screen recording
+//        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+//        enableEdgeToEdge()
+//
+//        // Inflate the layout using view binding
+//        binding = ActivityDashboardUserBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        firebaseAuth = FirebaseAuth.getInstance()
+//
+//        setupWithViewpager(binding.viewPager)
+//        binding.tablayout.setupWithViewPager(binding.viewPager)
+//    }
+//
+//    private fun setupWithViewpager(viewPager: ViewPager) {
+//        viewPagerAdapter = ViewPagerAdapter(
+//            supportFragmentManager,
+//            FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT,
+//            this
+//        )
+//        categoryArrayList = ArrayList()
+//
+//        val ref = FirebaseDatabase.getInstance().getReference("Category")
+//        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                categoryArrayList.clear()
+//                val modelAll = ModelCategorey("01", "All", 1, "")
+//                val modelMostViewed = ModelCategorey("02", "Most viewed", 1, "")
+//                val modelMostDownloaded = ModelCategorey("03", "Most Downloaded", 1, "")
+//
+//                categoryArrayList.add(modelAll)
+//                categoryArrayList.add(modelMostViewed)
+//                categoryArrayList.add(modelMostDownloaded)
+//
+//                viewPagerAdapter.addFragment(
+//                    BooksUserFragment.newInstance(
+//                        "${modelAll.id}",
+//                        "${modelAll.category}",
+//                        "${modelAll.uid}"
+//                    ), modelAll.category
+//                )
+//
+//                viewPagerAdapter.addFragment(
+//                    BooksUserFragment.newInstance(
+//                        "${modelMostViewed.id}",
+//                        "${modelMostViewed.category}",
+//                        "${modelMostViewed.uid}"
+//                    ), modelMostViewed.category
+//                )
+//
+//                viewPagerAdapter.addFragment(
+//                    BooksUserFragment.newInstance(
+//                        "${modelMostDownloaded.id}",
+//                        "${modelMostDownloaded.category}",
+//                        "${modelMostDownloaded.uid}"
+//                    ), modelMostDownloaded.category
+//                )
+//
+//                viewPagerAdapter.notifyDataSetChanged()
+//
+//                for (ds in snapshot.children) {
+//                    val model = ds.getValue(ModelCategorey::class.java)
+//                    categoryArrayList.add(model!!)
+//
+//                    viewPagerAdapter.addFragment(
+//                        BooksUserFragment.newInstance(
+//                            "${model.id}",
+//                            "${model.category}",
+//                            "${model.uid}"
+//                        ), model.category
+//                    )
+//
+//                    viewPagerAdapter.notifyDataSetChanged()
+//                }
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                // Handle error
+//            }
+//        })
+//
+//        viewPager.adapter = viewPagerAdapter
+//    }
+//
+//    class ViewPagerAdapter(fm: FragmentManager, behavior: Int,  context: Context) : FragmentPagerAdapter(fm, behavior) {
+//        private val fragmentList: ArrayList<BooksUserFragment> = ArrayList()
+//        private val fragmentTitleList: ArrayList<String> = ArrayList()
+//        private val context : Context
+//
+//        init {
+//            this.context = context
+//        }
+//
+//        override fun getCount(): Int {
+//            return fragmentList.size
+//        }
+//
+//        override fun getItem(position: Int): Fragment {
+//            return fragmentList[position]
+//        }
+//
+//        override fun getPageTitle(position: Int): CharSequence {
+//            return fragmentTitleList[position]
+//        }
+//
+//        fun addFragment(fragment: BooksUserFragment, title: String) {
+//            fragmentList.add(fragment)
+//            fragmentTitleList.add(title)
+//        }
+//    }
+//}
+
+
 package com.smartloopLearn.learning.admin
 
 import android.content.Context
@@ -10,26 +150,22 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.smartloopLearn.learning.databinding.ActivityDashboardUserBinding
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 
 class DashboardUserActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDashboardUserBinding
     private lateinit var categoryArrayList: ArrayList<ModelCategorey>
     private lateinit var viewPagerAdapter: ViewPagerAdapter
-    private lateinit var  firebaseAuth : FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Prevent screenshots and screen recording
+
         window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
         enableEdgeToEdge()
 
-        // Inflate the layout using view binding
         binding = ActivityDashboardUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -51,72 +187,57 @@ class DashboardUserActivity : AppCompatActivity() {
         ref.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 categoryArrayList.clear()
+
                 val modelAll = ModelCategorey("01", "All", 1, "")
-//                val modelMostViewed = ModelCategorey("02", "Most viewed", 1, "")
-//                val modelMostDownloaded = ModelCategorey("03", "Most Downloaded", 1, "")
+                val modelMostViewed = ModelCategorey("02", "Most Viewed", 1, "")
+                val modelMostDownloaded = ModelCategorey("03", "Most Downloaded", 1, "")
 
                 categoryArrayList.add(modelAll)
-//                categoryArrayList.add(modelMostViewed)
-//                categoryArrayList.add(modelMostDownloaded)
+                categoryArrayList.add(modelMostViewed)
+                categoryArrayList.add(modelMostDownloaded)
 
+                // Add fixed tabs
                 viewPagerAdapter.addFragment(
-                    BooksUserFragment.newInstance(
-                        "${modelAll.id}",
-                        "${modelAll.category}",
-                        "${modelAll.uid}"
-                    ), modelAll.category
+                    BooksUserFragment.newInstance(modelAll.id, modelAll.category, modelAll.uid),
+                    modelAll.category
+                )
+                viewPagerAdapter.addFragment(
+                    BooksUserFragment.newInstance(modelMostViewed.id, modelMostViewed.category, modelMostViewed.uid),
+                    modelMostViewed.category
+                )
+                viewPagerAdapter.addFragment(
+                    BooksUserFragment.newInstance(modelMostDownloaded.id, modelMostDownloaded.category, modelMostDownloaded.uid),
+                    modelMostDownloaded.category
                 )
 
-//                viewPagerAdapter.addFragment(
-//                    BooksUserFragment.newInstance(
-//                        "${modelMostViewed.id}",
-//                        "${modelMostViewed.category}",
-//                        "${modelMostViewed.uid}"
-//                    ), modelMostViewed.category
-//                )
-
-//                viewPagerAdapter.addFragment(
-//                    BooksUserFragment.newInstance(
-//                        "${modelMostDownloaded.id}",
-//                        "${modelMostDownloaded.category}",
-//                        "${modelMostDownloaded.uid}"
-//                    ), modelMostDownloaded.category
-//                )
-
-                viewPagerAdapter.notifyDataSetChanged()
-
+                // Add categories from database
                 for (ds in snapshot.children) {
                     val model = ds.getValue(ModelCategorey::class.java)
-                    categoryArrayList.add(model!!)
+                    if (model != null) {
+                        categoryArrayList.add(model)
 
-                    viewPagerAdapter.addFragment(
-                        BooksUserFragment.newInstance(
-                            "${model.id}",
-                            "${model.category}",
-                            "${model.uid}"
-                        ), model.category
-                    )
-
-                    viewPagerAdapter.notifyDataSetChanged()
+                        viewPagerAdapter.addFragment(
+                            BooksUserFragment.newInstance(model.id, model.category, model.uid),
+                            model.category
+                        )
+                    }
                 }
+
+                viewPagerAdapter.notifyDataSetChanged()
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                // Handle error
-            }
+            override fun onCancelled(error: DatabaseError) {}
         })
 
         viewPager.adapter = viewPagerAdapter
     }
 
-    class ViewPagerAdapter(fm: FragmentManager, behavior: Int,  context: Context) : FragmentPagerAdapter(fm, behavior) {
-        private val fragmentList: ArrayList<BooksUserFragment> = ArrayList()
-        private val fragmentTitleList: ArrayList<String> = ArrayList()
-        private val context : Context
+    class ViewPagerAdapter(fm: FragmentManager, behavior: Int, context: Context) :
+        FragmentPagerAdapter(fm, behavior) {
 
-        init {
-            this.context = context
-        }
+        private val fragmentList = ArrayList<BooksUserFragment>()
+        private val fragmentTitleList = ArrayList<String>()
+        private val context: Context = context
 
         override fun getCount(): Int {
             return fragmentList.size
